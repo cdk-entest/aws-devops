3a609aa349009418006cba0c1b640f6bb0b1237a
Tran Minh Hai <hai@bio-rithm.com>
2022-01-31 22:45:20 +0700

    add api gateway integration

--- aws_devops/aws_devops_stack.py
+++ aws_devops/aws_devops_stack.py
@@ -8,14 +8,16 @@ Option 3. build ecg image from local asset
 
 
 from os import path
+from wsgiref import handlers
 from aws_cdk import (
     Stack,
     Duration,
     aws_lambda,
     aws_apigateway,
-    CfnOutput
+    CfnOutput,
 )
 from constructs import Construct
+
 
 class AwsDevopsStack(Stack):
     """
@@ -28,24 +30,62 @@ class AwsDevopsStack(Stack):
         #
         dirname = path.dirname(__file__)
         # option 1. lambda handler without dependencies
-        handler = aws_lambda.Function(
+        handler_file = aws_lambda.Function(
             self,
             id="lambda-handler-wo-dependencies",
             code=aws_lambda.Code.from_asset(path.join(dirname, "lambda")),
-            handler="handler.handler",
+            handler="handler.handler_file",
             runtime=aws_lambda.Runtime.PYTHON_3_8,
             memory_size=512,
-            architecture=aws_lambda.Architecture.ARM_64,
             timeout=Duration.seconds(90)
         )
         # option 2
         # python -m pip install --target /lambda/ numpy
-
-        # api gateway
-        api_gw = aws_apigateway.LambdaRestApi(
+        handler_ecr = aws_lambda.Function(
             self,
-            id="api-lambda-handler-wo-dependencies",
-            handler=handler
+            id="lambda-ecr-build-local",
+            code=aws_lambda.EcrImageCode.from_asset_image(
+                directory=path.join(dirname, "lambda")
+            ),
+            handler=aws_lambda.Handler.FROM_IMAGE,
+            runtime=aws_lambda.Runtime.FROM_IMAGE,
+            memory_size=512,
+            timeout=Duration.seconds(90)
+        )
+
+        # create an api gateway
+        api_gw = aws_apigateway.RestApi(
+            self,
+            id="ApiGatewayLambdaDeployOptions",
+            rest_api_name="api-lambda-deploy-options"
+        )
+        # integrate lambda-file with api gateway
+        # create api resource for lambbda-file
+        api_file_resource = api_gw.root.add_resource(
+            path_part="file"
+        )
+        # lambad integration with api file
+        api_file_intetgration = aws_apigateway.LambdaIntegration(
+            handler=handler_file
+        )
+        # add method to the api file resource
+        api_file_resource.add_method(
+            http_method="GET",
+            integration=api_file_intetgration
+        )
+        # integrate lambda-ecr with api gateway
+        # create api resource for lambda-ecr
+        api_ecr_resource = api_gw.root.add_resource(
+            path_part="ecr"
+        )
+        # lambda integration with api ecr
+        api_ecr_integration = aws_apigateway.LambdaIntegration(
+            handler=handler_ecr
+        )
+        # add method to the api ecr resource
+        api_ecr_resource.add_method(
+            http_method="GET",
+            integration=api_ecr_integration
         )
         # get api url
         self.output_url = CfnOutput(self, id="Url", value=api_gw.url)
--- aws_devops/lambda/handler.py
+++ aws_devops/lambda/handler.py
@@ -5,17 +5,24 @@ Lambda handler
 
 import json
 import numpy as np
+from datetime import datetime
 
 
-def handler(event, context):
+def handler_file(event, context):
     """
     """
     # generate data
     sig = np.random.randint(0, 1000, (4096, 60))
-    # fft
+    # start time
+    start = datetime.now()
+    # perform fft
     y = np.fft.fft(sig, axis=0)
     # power spectrum sum
     p = np.sum(np.abs(y))
+    # end time
+    end = datetime.now()
+    # delta time
+    delta_time = end.timestamp() - start.timestamp()
     # return
     return {
         'statusCode': 200,
@@ -24,14 +31,51 @@ def handler(event, context):
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "OPTIONS,GET"
         },
-        'body': json.dumps({"sum spectrum": "{0}".format(p)},
-                           indent=4,
-                           sort_keys=True,
-                           default=str)
+        'body': json.dumps({
+            "api": "file",
+            "duration": "{0}".format(delta_time * 1000),
+            "sum spectrum": "{0}".format(p)
+        },
+            indent=4,
+            sort_keys=True,
+            default=str)
     }
 
 
+def handler(event, context):
+    """
+    """
+    # generate data
+    sig = np.random.randint(0, 1000, (4096, 60))
+    # start time
+    start = datetime.now()
+    # perform fft
+    y = np.fft.fft(sig, axis=0)
+    # power spectrum sum
+    p = np.sum(np.abs(y))
+    # end time
+    end = datetime.now()
+    # delta time
+    delta_time = end.timestamp() - start.timestamp()
+    # return
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
+        },
+        'body': json.dumps({
+            "api": "ecr",
+            "duration": "{0}".format(delta_time * 1000),
+            "sum spectrum": "{0}".format(p)
+        },
+            indent=4,
+            sort_keys=True,
+            default=str)
+    }
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     res = handler(event=None, context=None)
     print(res)
