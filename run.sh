9107e5b6667ff65ff707785e32e6ca8d956c6daa
Tran Minh Hai <hai@bio-rithm.com>
2022-02-03 13:46:21 +0700

    first

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 aws cloudformation create-stack \
- --stack-name cfn-lambda-demo \
- --template-body file://lambda_template.yaml \
+ --stack-name cfn-codepipeline-demo \
+ --template-body file://cicd.yaml \
  --capabilities CAPABILITY_NAMED_IAM
 
 
 aws cloudformation update-stack \
- --stack-name cfn-lambda-demo \
- --template-body file://lambda_template.yaml \
+ --stack-name cfn-codepipeline-demo \
+ --template-body file://cicd.yaml \
  --capabilities CAPABILITY_NAMED_IAM
+
+
+aws cloudformation delete-stack \
+  --stack-name cfn-lcodepipeline-demo
