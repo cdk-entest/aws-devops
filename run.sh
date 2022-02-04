692a6093bf4f9ad21bf7c5c5826ab02994c57420
Tran Minh Hai <hai@bio-rithm.com>
2022-02-04 20:17:58 +0700

    update run

--- run.sh
+++ run.sh
@@ -2,3 +2,13 @@ aws cloudformation create-stack \
  --stack-name cfn-lambda-demo \
  --template-body file://lambda_template.yaml \
  --capabilities CAPABILITY_NAMED_IAM
+
+ aws cloudformation create-stack \
+ --stack-name cfn-lambda-demo \
+ --template-body file://lambda_template.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --role-arn arn:aws:iam::610770234379:role/cdk-admin-role-CdkAdminRole-1IBORD9375GQF
+
+
+aws cloudformation delete-stack \
+ --stack-name cfn-lambda-demo
